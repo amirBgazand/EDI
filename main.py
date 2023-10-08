95415033b3130b2fdb0c19b7042c660402fcd134
amirBgazand <amirghafari44@gmail.com>
2023-10-09 02:00:55 +0330

    refactor(reading_dataset): Organize and improve code structure

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from functions_denoise import *
-from functions_compare import *
-from reading_dataset import *
+import matplotlib.pyplot as plt
+from functions_compare import ecg2twoD , mean_denoise, reproduce_leads_from_denoise
+from reading_dataset import read_ecg , plot_mat
 
-def improve_limb_leads (denoised_limb_leads_mat):
-    shape = denoised_limb_leads_mat.shape
-    assert shape[0] ==6
-    listing=ecg2twoD(denoised_limb_leads_mat)
-    x1,y1=mean_denoise(listing)
-    reconstructed_ecg=reproduce_leads_from_denoise(x1,y1,denoised_limb_leads_mat)
+def improve_limb_leads(denoised_limb_leads_mat):
+    assert denoised_limb_leads_mat.shape[0] == 6
+    listing = ecg2twoD(denoised_limb_leads_mat)
+    x1, y1 = mean_denoise(listing)
+    reconstructed_ecg = reproduce_leads_from_denoise(x1, y1, denoised_limb_leads_mat)
     return reconstructed_ecg
 
 if __name__ == "__main__":
-    ecg=read_ecg(1)
-    mat=ecg.mat [0:6,:]
-    plot_mat(improve_limb_leads(mat), just_limb=True)
+    ecg = read_ecg(1)
+    mat = ecg.mat[0:6, :]
+    improved_ecg = improve_limb_leads(mat)
+    plot_mat(improved_ecg, just_limb=True)
     plt.show()
