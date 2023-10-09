8e60cd894b08a9000db8b69f03653aaf477351de
amirBgazand <amirghafari44@gmail.com>
2023-10-09 03:46:22 +0330

    refactor: organize and improve code structure

--- reading_dataset.py
+++ reading_dataset.py
@@ -4,14 +4,11 @@ import os
 import matplotlib.pyplot as plt
 import scipy.io
 import numpy as np
-
-
-
-pkl_folder='pkls\\'
-
+import pickle as pkl
 
 
 class data ():
+    """Class to represent ECG data."""
     def __init__(self,index,mat,age, sex,Dx,samples
     ,sample_rate,Rx=None,Hx=None,Sx=None,folder=None) :
 
@@ -31,10 +28,6 @@ class data ():
             self.duration=self.samples /self.sample_rate
         return self.duration 
 
-   
-    
-
-
 def intersection(lst1, lst2):
     lst3 = [value for value in lst1 if value in lst2]
     return lst3
@@ -47,7 +40,7 @@ def get_location(folder_number):
     pishvand=pishvands[folder_number]
     zfilling_number= [5 if folder_number==5 or folder_number==4 else 4][0] 
     return location , pishvand , zfilling_number
-
+   
 
 def get_ecg(index,folder_number):
     location , pishvand , zfilling_number=get_location(folder_number)
@@ -57,12 +50,11 @@ def get_ecg(index,folder_number):
 
 
 def read_ecg(index,folder_number=5 ,):
+    """Read ECG data from files and create an ECGData object."""
     ecg_mat_file,ecg_annotation_file=get_ecg(index,folder_number)
     mat = scipy.io.loadmat(ecg_mat_file)
     np.seterr(invalid='ignore')
     mat=mat['val']
-    
-    
 
     with open (ecg_annotation_file, 'r') as file:
         
@@ -85,119 +77,42 @@ def read_ecg(index,folder_number=5 ,):
 
     return ecg
 
-    
-def plot_ecg_form(ecg , mat,time, form,just_limb=False):
-    dx=ecg.dx
-    long=mat.shape[1]
-    
-    x=(np.arange(ecg.samples, )/ecg.sample_rate)[:long]
-    y=mat/1000
-    y_of_time=y[:,int(time[0]*ecg.sample_rate):int(time[1]*ecg.sample_rate)]
-    maxy=y_of_time.max()
-    miny=y_of_time.min()
-    
-    if form == 'normal':
-        leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
-    else:         
-        y_new=np.empty((12,long))
-        y_new[0,:]=mat[4,:]
-        y_new[1,:]=mat[0,:]
-        y_new[2,:]=-mat[3,:]
-        y_new[3,:]=mat[1,:]
-        y_new[4,:]=mat[5,:]
-        y_new[5,:]=mat[2,:]
-        if just_limb==False:   
-            y_new[6:12,:]=mat[6:12,:]
-            
-        
-        leads =['aVL','I','-aVR','II','aVF','III','V1','V2','V3','V4','V5','V6']
-        mat=y_new
-    if just_limb==False:   
-        fig, ax = plt.subplots(6,2) 
-        for i in range (6):
-            for j in range (2):
-                y=mat[i+6*j,:]/1000
-                ax[i,j].plot(x,y)
-                ax[i,j].set_ylabel (leads[i+6*j]  )
-                
 
-                
-                tool=ecg.duration
-                small_vertical=np.arange(0,tool ,0.04)  
-                big_vertical=np.arange(0,tool,0.2)
-
-                [ax[i,j].axvline(x=k, linestyle='--',linewidth=0.2 ) for k in small_vertical]
-                [ax[i,j].axvline(x=k, linestyle='--',linewidth=0.5 ) for k in big_vertical]
-
-                small_horizontal=np.arange(-3,+3 ,0.1) 
-                big_horizontal=np.arange(-3,+3,0.5)
-                [ax[i,j].axhline(y=k, linestyle='--',linewidth=0.2 ) for k in small_horizontal]
-                [ax[i,j].axhline(y=k, linestyle='--',linewidth=0.5 ) for k in big_horizontal]
-                ax[i,j].axis([time[0], time[1],miny,maxy])
-    else:
-        fig, ax = plt.subplots(6)
-        j=0
-        for i in range (6):
-            y=mat[i,:]/1000
-            ax[i].plot(x,y)
-            ax[i].set_ylabel(leads[i] , fontdict={'fontfamily':'serif' ,'fontsize':11, })
-            
-            
-            ax[i].set_yticklabels(ax[i].get_xticks(), rotation=0, size=9)
-            
-
-            if i != 5:
-                ax[i].set_xticks([])
-            
-                  
-
-            tool=ecg.duration
-            small_vertical=np.arange(0,tool ,0.04)  
-            big_vertical=np.arange(0,tool,0.2)
-            [ax[i].axvline(x=k, linestyle='--',linewidth=0.2 ) for k in small_vertical]
-            [ax[i].axvline(x=k, linestyle='--',linewidth=0.5 ) for k in big_vertical]
-
-            small_horizontal=np.arange(-3,+3 ,0.1) 
-            big_horizontal=np.arange(-3,+3,0.5)
-            [ax[i].axhline(y=k, linestyle='--',linewidth=0.2 ) for k in small_horizontal]
-            [ax[i].axhline(y=k, linestyle='--',linewidth=0.5 ) for k in big_horizontal]
-            ax[i].axis([time[0], time[1],miny,maxy])
-
-    # fig.suptitle(f'index:{ecg.index} , age:{ecg.age} , sex:{ecg.sex} , dx:{str(new)} ' , fontsize=9 )        
-    plt.subplots_adjust(left=0.11, bottom=0.1, right=0.95, top=0.95, wspace=.12, hspace=0.22)        
+def dx2diagnosis(dx):
+    diagnosis_dict=pkl.load(open('dataset\\dx.pkl' , 'rb'))
+    return (diagnosis_dict[dx[0]])
 
 
-
-def plot_ecg (ecg,time=None,channels=None,folder_number=5 , form='better', just_limb=False):
+def plot_ecg (ecg,time=None,channels=None ,folder_number=5 
+              , form='better', just_limb=False,subtitle=True):
     
     mat=ecg.mat
-    
-    
+       
     if time != None:
         assert type(time)==tuple  
     if channels != None:
         subplot(channels,ecg,mat)
     else:
-        plot_ecg_form(ecg,mat,time ,form , just_limb=just_limb)
-        
-def plot_mat (mat, time=None, form='better', sample_rate=500, just_limb=False):
+        plot_mat(mat,time ,form , just_limb=just_limb)
+        if subtitle ==True:
+            diagnosis=dx2diagnosis(ecg.dx)
+            plt.suptitle(f'index:{ecg.index} , age:{ecg.age} , sex:{ecg.sex} , dx:{str(diagnosis)} '
+                          , fontsize=9 )
+
+
+def plot_mat (mat, time=None, custom_order=True, sample_rate=500, just_limb=False):
     
     samples= mat.shape[1]
     long=samples/sample_rate
     if time==None:
         time=(0,long)
-        
-    
-    
     x=(np.arange(samples, )/sample_rate)[:samples]
     y=mat/1000
     y_of_time=y[:,int(time[0]*sample_rate):int(time[1]*sample_rate)]
     maxy=y_of_time.max()
     miny=y_of_time.min()
     
-    if form == 'normal':
-        leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
-    else:         
+    if custom_order == True:
         y_new=np.empty((12,samples))
         y_new[0,:]=mat[4,:]
         y_new[1,:]=mat[0,:]
@@ -208,20 +123,19 @@ def plot_mat (mat, time=None, form='better', sample_rate=500, just_limb=False):
         if just_limb==False:   
             y_new[6:12,:]=mat[6:12,:]
             
-        
         leads =['aVL','I','-aVR','II','aVF','III','V1','V2','V3','V4','V5','V6']
         mat=y_new
+    else:    
+        leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']     
+        
     if just_limb==False:   
         fig, ax = plt.subplots(6,2) 
         for i in range (6):
             for j in range (2):
                 y=mat[i+6*j,:]/1000
                 ax[i,j].plot(x,y)
-                ax[i,j].set_ylabel (leads[i+6*j]  )
-                
+                ax[i,j].set_ylabel (leads[i+6*j]  )              
 
-                
-                tool=ecg.duration
                 small_vertical=np.arange(0,long ,0.04)  
                 big_vertical=np.arange(0,long,0.2)
 
@@ -240,16 +154,9 @@ def plot_mat (mat, time=None, form='better', sample_rate=500, just_limb=False):
             y=mat[i,:]/1000
             ax[i].plot(x,y)
             ax[i].set_ylabel(leads[i] , fontdict={'fontfamily':'serif' ,'fontsize':11, })
-            
-            
             ax[i].set_yticklabels(ax[i].get_xticks(), rotation=0, size=9)
-            
-
             if i != 5:
                 ax[i].set_xticks([])
-            
-                  
-
             
             small_vertical=np.arange(0,long ,0.04)  
             big_vertical=np.arange(0,long,0.2)
@@ -262,20 +169,18 @@ def plot_mat (mat, time=None, form='better', sample_rate=500, just_limb=False):
             [ax[i].axhline(y=k, linestyle='--',linewidth=0.5 ) for k in big_horizontal]
             ax[i].axis([time[0], time[1],miny,maxy])
 
-    # fig.suptitle(f'index:{ecg.index} , age:{ecg.age} , sex:{ecg.sex} , dx:{str(new)} ' , fontsize=9 )        
     plt.subplots_adjust(left=0.11, bottom=0.1, right=0.95, top=0.95, wspace=.12, hspace=0.22)       
 
 
 
 def subplot (channels , ecg , mat):
-    leads=['I','II','III','aVR','aVL','aVF','V1','V2','V3','V4','V5','V6']
+    leads=['I','II','III','aVR','aVL','aVF'
+           ,'V1','V2','V3','V4','V5','V6']
     x=np.arange(ecg.samples, )/ecg.sample_rate
-    
     
     if type(channels)!=list and channels!=None :
         plt.plot(x,mat [channels ,:]/1000)
         plt.ylabel(leads[channels])
-       
     else:    
         if channels==None:
             channels=[i for i in range(12)]
@@ -306,14 +211,11 @@ def subplot (channels , ecg , mat):
             
         fig.suptitle(f'index:{ecg.index} , age:{ecg.age} , sex:{ecg.sex}  ' , fontsize=9 )
 
-# plot_ecg (13,(0,6) )
-
-
-
 
 def find_normals(folder_number=5):
     location=get_location(folder_number)[0]
-    number_of_files=int (len([name for name in os.listdir(location) if os.path.isfile(os.path.join(location, name))])/2)
+    number_of_files=int (len([name for name in os.listdir(location) 
+                              if os.path.isfile(os.path.join(location, name))])/2)
     count=0
     normal_numbers=[]
     for i in range (1,number_of_files+1):
@@ -321,37 +223,4 @@ def find_normals(folder_number=5):
         if ecg.dx==['426783006']:
             normal_numbers.append(i)
             count+=1
-    return normal_numbers    , count    
-
-
- 
-
-
-
-def create_file(list_that_you_want,name_of_file,folder_number=5,just_index=False ,):
-
-    loc='seperated ecgs\\'
-    leng=len(list_that_you_want[0])
-    if just_index==False:
-        with open (f'{loc}file{folder_number} {name_of_file}.txt' , 'w') as f:
-            f.write(str(leng))
-            for item in list_that_you_want:
-                f.write(str(item))
-    else:
-        with open (f'{loc}file{folder_number}just index {name_of_file}.txt' , 'w') as f:
-            f.write(str(leng))
-            f.write(str(list_that_you_want[0])     )   
-
-
-def create_csv_file(list_that_you_want,name_of_file,folder_number=5 ,)  :            
-    loc='seperated ecgs\\'
-    pickle.dump(list_that_you_want,open(f'{loc}file{folder_number} {name_of_file}.pkl','wb'))
-    
-# def read_emd_ecg (index):
-#     loc='Denoise_matlab\\emd\\denoised_dataset\\'
-#     pishvand='emdE'
-#     zfilling_number=5
-#     emd_file=f'{loc}{pishvand}{(str(index)).zfill(zfilling_number)}.mat'
-#     mat = scipy.io.loadmat(emd_file)
-#     mat=mat['Final_ecg']
-#     return mat           
+    return normal_numbers , count    
